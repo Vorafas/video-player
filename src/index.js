3b7412c59d4f127eab1a162c55a2c3ee9859ef8b
Elshad <safarov.elshad007@gmail.com>
2019-05-22 19:55:54 +0500

    Automatic playback of the first video

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ const video = document.querySelector('#video-player');
 const url = "http://dash.edgesuite.net/dash264/TestCases/1a/sony/SNE_DASH_SD_CASE1A_REVISED.mpd";
 const player = dashjs.MediaPlayer().create();
 player.initialize(video, url, true);
+
+if ( video.src) {
+    video.then(_ => {
+        video.play();
+    }).catch(error => {
+        console.log('Autoplay was prevented', error);
+    });
+}
