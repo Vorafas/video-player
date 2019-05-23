e9b18ea9bf72be0a7e97019188edef85f00a55f5
Elshad <safarov.elshad007@gmail.com>
2019-05-23 17:21:32 +0500

    Added progressUpdate method. Added control button.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 
 const video = document.querySelector('#video-player');
 const playBtn = document.querySelector('#play-btn');
+const progress = document.querySelector('#seekbar-play');
 
 const url = "http://dash.edgesuite.net/dash264/TestCases/1a/sony/SNE_DASH_SD_CASE1A_REVISED.mpd";
 const player = dashjs.MediaPlayer().create();
@@ -19,3 +20,13 @@ if(video !== undefined){
         console.log('Autoplay was prevented', error);
     });
 }
+const progressUpdate = function(){
+    // console.log(video.duration);
+    // console.log(video.currentTime);
+    let d = video.duration;
+    let c = video.currentTime;
+    progress.style.width = `${(100 * c) / d}%`; 
+}
+
+video.ontimeupdate = progressUpdate;
+
