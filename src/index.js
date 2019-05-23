b6318143072623f60bf54b8e4586cea0fa58cc6f
Elshad <safarov.elshad007@gmail.com>
2019-05-23 16:26:15 +0500

    Changed autoplay of first video

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import dashjs from 'dashjs';
 import './index.css';
 
 const video = document.querySelector('#video-player');
+const playBtn = document.querySelector('#play-btn');
+
 const url = "http://dash.edgesuite.net/dash264/TestCases/1a/sony/SNE_DASH_SD_CASE1A_REVISED.mpd";
 const player = dashjs.MediaPlayer().create();
 player.initialize(video, url, true);
 
-if ( video.src) {
-    video.then(_ => {
-        video.play();
+if(video !== undefined){
+    video.play().then(_ => {
+        // Autoplay started! 
+        video.removeAttribute('muted');
+        playBtn.querySelector('.icon-btn').innerHTML = `<i class="fas fa-pause"></i>`;
     }).catch(error => {
+        // Autoplay was prevented.
         console.log('Autoplay was prevented', error);
     });
 }
